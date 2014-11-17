a0d40508300b885a9875e74f798db97a2865b926
Sebastian Raczkowski <sebastian.raczkowski@intel.com>
2014-11-17 22:33:03 +0100

    fix

--- GeoApplication/src/geo/geoapplication/Login.java
+++ GeoApplication/src/geo/geoapplication/Login.java
@@ -13,50 +13,52 @@ public class Login {
 	public Login(final String user, final String password) {
 		new Thread(new Runnable() {
             public void run() {
-                try{
-                	String md5Password = getMd5Hash(password);
-                    URL url = new URL(Connection.host +"/gis_server/LoginFromAndroid");
-                    
-                    URLConnection connection = url.openConnection();
-                    
-                    //inputString = URLEncoder.encode(inputString, "UTF-8");
-
-                    connection.setDoOutput(true);
-                    OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream());
-                    
-                    out.write(user+"|"+md5Password);
-                    out.close();
-
-                    BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-
-                    String returnString="";
-                    String result = "";
-                   
-                    while ((returnString = in.readLine()) != null) 
-                    {
-                        result += returnString;
-                    }
-                    in.close();
-
-                    if(result.contains("true")) {
-                    	GeoService.StaticData.logged = true;
-                    	GeoService.StaticData.user = user;
-                    	String verifyCode = "";
-                    	for(int i=4; i<result.length(); i++) {
-                    		verifyCode += result.charAt(i);
-                    	}
-                    	
-                    	GeoService.StaticData.verificationCode = verifyCode;
-                    }
-                    
-                    else {
-                    	GeoService.StaticData.user = "login error";
-                    }
-
-                }catch(Exception e)
-                {
-                	GeoService.StaticData.user = "connection error";
-                }
+            	if(!GeoService.StaticData.logged) {
+	                try{
+	                	String md5Password = getMd5Hash(password);
+	                    URL url = new URL(Connection.host +"/gis_server/LoginFromAndroid");
+	                    
+	                    URLConnection connection = url.openConnection();
+	                    
+	                    //inputString = URLEncoder.encode(inputString, "UTF-8");
+	
+	                    connection.setDoOutput(true);
+	                    OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream());
+	                    
+	                    out.write(user+"|"+md5Password);
+	                    out.close();
+	
+	                    BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+	
+	                    String returnString="";
+	                    String result = "";
+	                   
+	                    while ((returnString = in.readLine()) != null) 
+	                    {
+	                        result += returnString;
+	                    }
+	                    in.close();
+	
+	                    if(result.contains("true")) {
+	                    	GeoService.StaticData.logged = true;
+	                    	GeoService.StaticData.user = user;
+	                    	String verifyCode = "";
+	                    	for(int i=4; i<result.length(); i++) {
+	                    		verifyCode += result.charAt(i);
+	                    	}
+	                    	
+	                    	GeoService.StaticData.verificationCode = verifyCode;
+	                    }
+	                    
+	                    else {
+	                    	GeoService.StaticData.user = "login error";
+	                    }
+	
+	                }catch(Exception e)
+	                {
+	                	GeoService.StaticData.user = "connection error";
+	                }
+            	}
 
             }
           }).start();
